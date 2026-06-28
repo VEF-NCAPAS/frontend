a95bf7db700c14fe7f032e9f2caebf5ee890722a
EmelyChavez <00097923@uca.edu.sv>
2026-06-28 05:35:55 -0600

    :bug:: fix problems

--- WorkHive/vite/src/config/axiosConfig.js
+++ WorkHive/vite/src/config/axiosConfig.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL 
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const isApiRequest = error.config.url.includes('/api');
+    
+    if (error.response?.status === 401 && isApiRequest) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("role");
+      window.location.href = "/pages/login";
+    }
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    return Promise.reject(error);
   }
-
-  return config;
-});
+);
 
 api.interceptors.response.use(
   (response) => response,
